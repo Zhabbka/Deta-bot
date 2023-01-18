5f740067ffb59dfa4a3ac4f76de7f8235a8df429
Timur Zhabbarov <zhabbkaa@gmail.com>
2023-01-18 20:43:56 +0500

    женя тебе

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print('zhebs')
-print ('AMOGUS')
+import telebot
+
+bot = telebot.TeleBot('5947033600:AAF6rJihphkXcxkzAVQVYwZXkV8mUM1XDUY')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you govno?")
+
+bot.infinity_polling()
