886c6eb873a13b5138fb0a139bb5d6051d5c55ed
Timur Zhabbarov <zhabbkaa@gmail.com>
2023-01-26 15:25:17 +0500

    исправил добавил конопки ииии нужно как то сделать мемы и гороскоп

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-class MemeState(StatesGroup):
-    meme = State()
+class HoroState(StatesGroup):
+    horo = State()
 
 
 class WeatherState(StatesGroup):
@@ -21,18 +21,18 @@ token_open_weather = '8fff1770420e67a485593b01210eda78'
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 # Кнопки для клавы
-memes_button = KeyboardButton('/Мемы')
+horo_button = KeyboardButton('/Гороскоп')
 weather_button = KeyboardButton('/Погода')
 # Клава
 keyboard = ReplyKeyboardMarkup()
-keyboard.add(memes_button).add(weather_button)
+keyboard.add(horo_button).add(weather_button)
 
 
 # Регистрация команд
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(start_command, commands=['start'])
     dp.register_message_handler(weather, commands=['Погода'])
-    dp.register_message_handler(meme, commands=['Мемы'])
+    dp.register_message_handler(horo, commands=['Гороскоп'])
 
 
 @dp.message_handler(commands=["start"])
@@ -75,14 +75,43 @@ async def weather_answer(message: types.Message, state: FSMContext):
         await message.reply("Проверьте название города")
 
 
-@dp.message_handler(commands=['Мемы'])
-async def meme(message: types.Message):
-    await message.answer('Введите слово')
-    await MemeState.meme.set()
+@dp.message_handler(commands=['Гороскоп'])
+async def horo(message: types.Message):
+    await message.answer('Сейчас я расскажу тебе гороскоп на сегодня, выбери свой знак зодиака')
+    keyboard = types.InlineKeyboardMarkup()
+    # По очереди готовим текст и обработчик для каждого знака зодиака
+    key_oven = types.InlineKeyboardButton(text='Овен', callback_data='zodiac')
+    # И добавляем кнопку на экран
+    keyboard.add(key_oven)
+    key_telec = types.InlineKeyboardButton(text='Телец', callback_data='zodiac')
+    keyboard.add(key_telec)
+    key_bliznecy = types.InlineKeyboardButton(text='Близнецы', callback_data='zodiac')
+    keyboard.add(key_bliznecy)
+    key_rak = types.InlineKeyboardButton(text='Рак', callback_data='zodiac')
+    keyboard.add(key_rak)
+    key_lev = types.InlineKeyboardButton(text='Лев', callback_data='zodiac')
+    keyboard.add(key_lev)
+    key_deva = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
+    keyboard.add(key_deva)
+    key_vesy = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
+    keyboard.add(key_vesy)
+    key_scorpion = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
+    keyboard.add(key_scorpion)
+    key_strelec = types.InlineKeyboardButton(text='Стрелец', callback_data='zodiac')
+    keyboard.add(key_strelec)
+    key_kozerog = types.InlineKeyboardButton(text='Козерог', callback_data='zodiac')
+    keyboard.add(key_kozerog)
+    key_vodoley = types.InlineKeyboardButton(text='Водолей', callback_data='zodiac')
+    keyboard.add(key_vodoley)
+    key_ryby = types.InlineKeyboardButton(text='Рыбы', callback_data='zodiac')
+    keyboard.add(key_ryby)
+    # Показываем все кнопки сразу и пишем сообщение о выборе
+    await bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
 
 
-@dp.message_handler(state=MemeState.meme)
-async def meme_answer(message: types.Message, state: FSMContext):
+
+@dp.message_handler(state=HoroState.horo)
+async def horo_answer(message: types.Message, state: FSMContext):
     await message.answer(message.text)
     await state.finish()
 
